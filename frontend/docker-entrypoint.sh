5cce665c9bc43a028ffc3ccddef2c20f39937e40
Jiajun Zheng <zheng.jiaju@northeastern.edu>
2026-04-22 14:07:16 -0400

    fix: default frontend env vars before nginx startup

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+: "${PORT:=80}"
+: "${BACKEND_URL:=http://localhost:8000}"
+
 # Replace environment variables in nginx config
 envsubst '${PORT} ${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
