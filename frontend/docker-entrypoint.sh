1052eef83d47ef9f1ea4d4da1d2677bfa203eaf0
Jiajun Zheng <zheng.jiaju@northeastern.edu>
2026-04-22 17:18:15 -0400

    fix: bind frontend nginx to railway port

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-: "${PORT:=80}"
-: "${BACKEND_URL:=http://localhost:8000}"
+export PORT="${PORT:-80}"
+export BACKEND_URL="${BACKEND_URL:-http://localhost:8000}"
 
 # Replace environment variables in nginx config
 envsubst '${PORT} ${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
